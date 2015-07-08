8a5739b9c7320eaf8f9fe15fa2f494ba715811b0
terry408911 <519046658@qq.com>
2015-07-08 23:46:04 +0800

    add a python reptile to get the blog in my blog

--- myblog.py
+++ myblog.py
@@ -2,10 +2,7 @@
 
 import urllib2
 import re
-html_contents = urllib2.urlopen('http://terrynie.com/').read()
-links_info = re.findall('<li><a title=(.*?) rel=',html_contents,re.S)
-for every_link in links_info:
-    number = re.search('(\d+)',every_link,re.S).group(1)
-    links = re.search("/(.*?)'",every_link,re.S).group(1)
-    lesson_info = '第'+number +'集 地址：http://v.dxsbb.com/'+links
-    print lesson_info
+html=urllib2.urlopen('http://terrynie.com/').read()
+contents = re.findall('<h2 class="post-title"><a href="(.*?)">(.*?)</a>',html,re.S)#.group(1)
+for content in contents:
+    print "博客标题："+content[1]+"  http://terrynie.com"+content[0]
